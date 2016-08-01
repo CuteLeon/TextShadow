c5afdecf3f2e17c35d5993c09a5a961a6d3193c8
Leon <xiaoyankeji@hotmail.com>
2016-08-01 18:51:05 +0800

    第一次提交

---
+++ 文字阴影/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(200, 170)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "文字阴影"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(0, 167)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(196, 54)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "Leon.ID@QQ.COM" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Draw Shadow For Text."
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(200, 193)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "设置字体"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 12)
+        Me.Label1.TabIndex = 4
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 221)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("宋体", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Text Shadow"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FontDialog1 As FontDialog
+    Friend WithEvents Label1 As Label
+End Class
---
+++ 文字阴影/Form1.vb
@@ -0,0 +1,94 @@
+﻿Public Class Form1
+    Dim MyFont As Font = New Font("微软雅黑", 36.0)
+    Dim Radius As Integer = 10
+    Dim DoubleRadius As Integer = Radius * 2
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.AutoSize = True
+        Label1.Font = MyFont
+        Label1.Text = TextBox1.Text
+        Dim D1 As Double = My.Computer.Clock.TickCount
+        Label1.Image = TextShadowStroke(Label1.Text, MyFont, 10, Color.White, Color.Black, Color.Black, New Size(Label1.Width + DoubleRadius, Label1.Height + DoubleRadius))
+        Dim D2 As Double = My.Computer.Clock.TickCount
+        Label1.Text = vbNullString
+        Label1.AutoSize = False
+        Label1.Size = Label1.Image.Size
+        'MsgBox((D2 - D1) / 1000)
+    End Sub
+
+    Private Function TextShadowStroke(ByVal DrawText As String, ByVal TextFont As Font, ByVal ShadowRadius As Integer, ByVal ForeColor As Color, ByVal ShadowColor As Color, ByVal StrokeColor As Color, ByVal BitmapSize As Size) As Bitmap
+        Dim DoubleRadius As Integer = ShadowRadius * 2
+        Dim ShadowColorCell() As Byte = {ShadowColor.R, ShadowColor.G, ShadowColor.B}
+        Dim ResualtBitmap As Bitmap = New Bitmap(BitmapSize.Width, BitmapSize.Height)
+        Dim ResualtGraphics As Graphics = Graphics.FromImage(ResualtBitmap)
+        ResualtGraphics.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
+        ResualtGraphics.TextRenderingHint = Drawing.Text.TextRenderingHint.ClearTypeGridFit
+
+        '绘制阴影
+        ResualtGraphics.DrawString(DrawText, TextFont, New SolidBrush(ShadowColor), New PointF(ShadowRadius, ShadowRadius))
+        Dim ResualtBitmapData As Imaging.BitmapData = New Imaging.BitmapData
+        ResualtBitmapData = ResualtBitmap.LockBits(New Rectangle(0, 0, BitmapSize.Width, BitmapSize.Height), Imaging.ImageLockMode.WriteOnly, ResualtBitmap.PixelFormat)
+        Dim DataStride As Integer = ResualtBitmapData.Stride
+        Dim DataWidth As Integer = ResualtBitmapData.Width
+        Dim DataHeight As Integer = ResualtBitmapData.Height
+        Dim InitalDataArray(DataStride * DataHeight - 1) As Byte
+        Dim DataArray(DataStride * DataHeight - 1) As Byte
+        Dim Position(DoubleRadius, DoubleRadius) As Integer
+        Runtime.InteropServices.Marshal.Copy(ResualtBitmapData.Scan0, InitalDataArray, 0, InitalDataArray.Length)
+        Dim Index = 0, IndexX, IndexY As Integer, Round, RoundX, RoundY As Integer
+        Dim ByteSum, AValue As Integer
+        Dim Boundary(DataHeight) As Integer, LineIndex As Integer
+        For RoundY = 0 To DoubleRadius
+            Index = (RoundY - ShadowRadius) * DataWidth - ShadowRadius
+            For RoundX = 0 To DoubleRadius
+                Position(RoundY, RoundX) = IIf((RoundX - ShadowRadius) ^ 2 + (RoundY - ShadowRadius) ^ 2 <= ShadowRadius ^ 2, 4 * (Index + RoundX), 0)
+            Next
+        Next
+        For IndexY = 0 To DataHeight - 1
+            Boundary(IndexY + 1) = Boundary(IndexY) + DataStride
+        Next
+        For IndexY = 0 To DataHeight - 1
+            For IndexX = 0 To DataWidth - 1
+                ByteSum = 0 : AValue = 0
+                Index = IndexY * DataStride + IndexX * 4
+                For RoundY = 0 To DoubleRadius
+                    LineIndex = IndexY + RoundY - ShadowRadius
+                    If 0 <= LineIndex AndAlso LineIndex < Boundary.Count - 1 Then
+                        For RoundX = 0 To DoubleRadius
+                            Round = Index + Position(RoundY, RoundX)
+                            If Boundary(LineIndex) <= Round AndAlso Round < Boundary(LineIndex + 1) Then
+                                AValue += InitalDataArray(Round + 3)
+                                ByteSum += 1
+                            End If
+                        Next
+                    End If
+                Next
+                AValue /= ByteSum
+                DataArray(Index) = ShadowColorCell(2)
+                DataArray(Index + 1) = ShadowColorCell(1)
+                DataArray(Index + 2) = ShadowColorCell(0)
+                DataArray(Index + 3) = AValue
+            Next
+        Next
+        Runtime.InteropServices.Marshal.Copy(DataArray, 0, ResualtBitmapData.Scan0, DataArray.Length)
+        ResualtBitmap.UnlockBits(ResualtBitmapData)
+        '文字描边
+        Dim DrawBrush As Brush = New System.Drawing.Drawing2D.LinearGradientBrush(New Point(0, 0), New Point(0, 1), StrokeColor, StrokeColor)
+        ResualtGraphics.DrawString(DrawText, TextFont, DrawBrush, New Point(ShadowRadius - 1, ShadowRadius + 1))
+        ResualtGraphics.DrawString(DrawText, TextFont, DrawBrush, New Point(ShadowRadius - 1, ShadowRadius - 1))
+        ResualtGraphics.DrawString(DrawText, TextFont, DrawBrush, New Point(ShadowRadius + 1, ShadowRadius + 1))
+        ResualtGraphics.DrawString(DrawText, TextFont, DrawBrush, New Point(ShadowRadius + 1, ShadowRadius - 1))
+        ResualtGraphics.DrawString(DrawText, TextFont, DrawBrush, New Point(ShadowRadius + 1, ShadowRadius))
+        ResualtGraphics.DrawString(DrawText, TextFont, DrawBrush, New Point(ShadowRadius - 1, ShadowRadius))
+        ResualtGraphics.DrawString(DrawText, TextFont, DrawBrush, New Point(ShadowRadius, ShadowRadius + 1))
+        ResualtGraphics.DrawString(DrawText, TextFont, DrawBrush, New Point(ShadowRadius, ShadowRadius - 1))
+        '绘制原文字
+        ResualtGraphics.DrawString(DrawText, TextFont, New SolidBrush(ForeColor), New PointF(ShadowRadius, ShadowRadius))
+        ResualtGraphics.Dispose()
+        Return ResualtBitmap
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If FontDialog1.ShowDialog = DialogResult.OK Then MyFont = FontDialog1.Font
+    End Sub
+End Class
